3b3f2afd4cdad68d988474806a567d41be4b7e3a
Digvijay <digvijay@capiot.com>
2019-04-06 20:47:43 +0530

    finale page done

--- js/custom_jquery.js
+++ js/custom_jquery.js
@@ -41,3 +41,11 @@ function selectColor(value) {
     }
 
 }
+
+
+$(document).ready(function () {
+    $('#topheader .navbar-nav a').on('click', function () {
+        $('#topheader .navbar-nav').find('li.active').removeClass('active');
+        $(this).parent('li').addClass('active');
+    });
+});
